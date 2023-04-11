a12fbaa7eaf064f0817f67b3995954ea5fdeb06d
Kayla Dimaano <kayladimaano@campus-021-104.ucdavis.edu>
2023-04-11 14:25:43 -0700

    first update for portal

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 (function() {
     'use strict';
 
-    const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
-    let mode = 'dark';
 
-    button.addEventListener('click', function() {
-        if (mode === 'dark') {
-            body.className = 'switch';
-            banner.className = 'switch';
-            button.className = 'switch';
-            for (const section of sections) {
-                section.className = 'switch';
-            }
-            mode = 'light';
-        } else {
-            body.removeAttribute('class');
-            banner.removeAttribute('class');
-            button.removeAttribute('class');
-            for (const section of sections) {
-                section.removeAttribute('class');
-            }
-            mode = 'dark'
+    const heart = document.querySelector('#heart');
+    const spade = document.querySelector('#spade');
+
+
+    // Randomly display either GirlyPop or PunkPop when window loads
+    if (Math.floor(Math.random() * 2) === 0) {
+        girlyPop();
+    } else {
+        punkPop();
+    }
+
+    // Switches when spade/heart is clicked
+    spade.addEventListener('click', function(){
+        if (body.className = 'punkPop') {
+            girlyPop();
+        } 
+        
+    })
+    heart.addEventListener('click', function(){
+        if (body.className = 'girlyPop') {
+            punkPop();
         }
     })
+
+    
+    // Theme Functions
+    function girlyPop() {
+        body.className = 'girlyPop';
+        spade.style.display = 'none';
+        heart.style.display = 'block';
+        banner.src = 'images/girlyPopBanner.jpg';
+    }
+    function punkPop() {
+        body.className = 'punkPop';
+        heart.style.display = 'none';
+        spade.style.display = 'block';
+        banner.src = 'images/punkPopBanner.jpg';
+    }
+
+
 })()
