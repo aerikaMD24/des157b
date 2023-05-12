19d056b00562409021554ebb4098cbc4ebc65753
Kayla Dimaano <kayladimaano@Kaylas-Air-5.hsd1.ca.comcast.net>
2023-05-11 21:00:10 -0700

    lib liberty

--- studio3/script.js
+++ studio3/script.js
@@ -10,16 +10,52 @@
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' 
     }).addTo(map);
 
-    var marker = L.marker([38.5343, -121.7492]).addTo(map);
-    marker.bindPopup("Mondavi Center");
+    // test buttons w/i popupss for temporary local storage testing
 
-    
+    var marker = L.marker([38.5343, -121.7492]).addTo(map);
+    marker.bindPopup('Mondavi Center <button id="clear">clear</button>');
 
     var marker = L.marker([38.531, -121.76]).addTo(map);
-    marker.bindPopup("Arboretum");
+    marker.bindPopup('Arboretum <button id="local">test</button>');
     
     var marker = L.marker([38.5377, -121.7494]).addTo(map);
     marker.bindPopup("'Eye on Mrak' Egghead");
+
+
+
+
+
+
+    // LOCAL STORAGE TEST RUN HERE
+
+    // If no local storage data has been set, default to false
+    if (localStorage.getItem('bool') == null) {
+        localStorage.setItem('bool', 'false');
+    }
+
+    // Print to console whether local storage data was stored
+    function loca() { 
+        console.log('you clicked test,', localStorage.getItem('bool'));
+    }
+    // Call function when window first opened
+    loca();
+
+    
+    document.addEventListener('click', function(e){
+        //  Change boolean to true when test button clicked
+        if (e.target.matches('#local')) {
+            localStorage.setItem('bool', 'true');
+            loca();
+        }
+
+        // Clear local storage data completely
+        if (e.target.matches('#clear')) {
+            // console.log('clear clicked');
+            loca();
+            localStorage.clear();
+        }
+
+    }, false)
 
     
 }());
